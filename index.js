af35342c4528f85a7f975b629de6587a849e0e4b
jcv12 <john.c.vayianos@gmail.com>
2022-04-23 15:54:10 -0400

    fixed up some errors

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
+const fetch = require("node-fetch");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const ghUserApi = require("./utils/githubUser.js");
+const writeFile = require('./utils/generateMarkdownFile.js');
 
-
-
-
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "userName",
-            message: "What is your Github Username?",
-            validate: (accountNameInput) => {
-                if(accountNameInput) {
-                    return true;
-                } else {
-                    console.log("Enter your Github username!");
-                    return false;
-                }
-            },
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your Github email",
-            validate: (emailNameInput) => {
-                if(emailNameInput) {
-                    return true;
-                } else {
-                    console.log("Enter your email!");
-                    return false;
-                }
-            },
-        },
-        {
-            type: "input",
+// array of questions for user
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "accountName",
+      message: "What is your github username? (Required)",
+      validate: (accountNameInput) => {
+        if(accountNameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "accountEmail",
+      message: "What is your github email address? (Required)",
+      validate: (accountEmailInput) => {
+        if(accountEmailInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub email address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
       name: "repoName",
       message: "What is the name of the repository eg. repo-name? (Required)",
       validate: (projectTitleInput) => {
@@ -130,108 +132,108 @@ const questions = () => {
       default: true,
     },
     {
-        type: "confirm",
-        name: "confirmTest",
-        message:
-          "Would you like to enter information on how is this project tested??",
-        default: false,
+      type: "confirm",
+      name: "confirmTest",
+      message:
+        "Would you like to enter information on how is this project tested??",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "How is this project tested?:",
+      when: ({ confirmTest }) => {
+        if (confirmTest) {
+          return true;
+        } else {
+          return false;
+        }
       },
-      {
-        type: "input",
-        name: "test",
-        message: "How is this project tested?:",
-        when: ({ confirmTest }) => {
-          if (confirmTest) {
-            return true;
-          } else {
-            return false;
-          }
-        },
+    },
+    {
+      type: "confirm",
+      name: "confirmContribute",
+      message:
+        "Would you like to enter some information about how can someone contribute to this project?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: "How can someone contribute to this project?:",
+      when: ({ confirmContribute }) => {
+        if (confirmContribute) {
+          return true;
+        } else {
+          return false;
+        }
       },
-      {
-        type: "confirm",
-        name: "confirmContribute",
-        message:
-          "Would you like to enter some information about how can someone contribute to this project?",
-        default: false,
-      },
-      {
-        type: "input",
-        name: "contribute",
-        message: "How can someone contribute to this project?:",
-        when: ({ confirmContribute }) => {
-          if (confirmContribute) {
-            return true;
-          } else {
-            return false;
-          }
-        },
-      },
-      {
-        type: "confirm",
-        name: "image",
-        message: "Would you like to display your user image?",
-        default: false,
-      },
-    ]);
-  };
+    },
+    {
+      type: "confirm",
+      name: "image",
+      message: "Would you like to display your user image?",
+      default: false,
+    },
+  ]);
+};
 
+
+// function to initialize program
 function init() {
-    function init() {
-        var data = {};
-        promptUser()
-          .then((answers) => {
-            data = answers;
-            return answers;
-          })
-          .then(ghUserData => {
-            const ghUserTemp = ghUserApi.getUser(ghUserData.accountName);
-            return ghUserTemp;
-          })
-          .then(temData => {
-            data.imageUrl = temData.data.avatar_url;
-            data.userFullName = temData.data.name;
-          })
-          .then(getRepoData =>{
-            const repData = fetch('https://api.github.com/repos/'+data.accountName+'/'+data.repoName)
-            .then((response) => {
-              return response.json();
-            })
-            .then((rData) => {
-              return rData;
-            })
-            .then(compInfo => {
-              data.licenseInfo = compInfo.license;
-            })
-            .then(licenseDetail => {
-              const licDetail = fetch(data.licenseInfo.url)
-              .then((response) => {
-                return response.json();
-              })
-              .then((lData) => {
-                return lData;
-              })
-              .then(compLInfo => {
-                data.licenseDetail = compLInfo;
-               /*  console.log(data); */
-              })
-              .then(generateMD => {
-                /* console.log(data); */
-                return generateMarkdown(data);
-              })
-              .then(pageReadme => {
-                return writeFile(pageReadme);
-              })
-              .then(writeFileResponse =>{
-                console.log(writeFileResponse.message);
-              })
-              .catch(err => {
-                console.log(err);
-              });
-            });
-          });
-      }
-      
-      // function call to initialize program
-      init();
+  var data = {};
+  promptUser()
+    .then((answers) => {
+      data = answers;
+      return answers;
+    })
+    .then(ghUserData => {
+      const ghUserTemp = ghUserApi.getUser(ghUserData.accountName);
+      return ghUserTemp;
+    })
+    .then(temData => {
+      data.imageUrl = temData.data.avatar_url;
+      data.userFullName = temData.data.name;
+    })
+    .then(getRepoData =>{
+      const repData = fetch('https://api.github.com/repos/'+data.accountName+'/'+data.repoName)
+      .then((response) => {
+        return response.json();
+      })
+      .then((rData) => {
+        return rData;
+      })
+      .then(compInfo => {
+        data.licenseInfo = compInfo.license;
+      })
+      .then(licenseDetail => {
+        const licDetail = fetch(data.licenseInfo.url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((lData) => {
+          return lData;
+        })
+        .then(compLInfo => {
+          data.licenseDetail = compLInfo;
+         /*  console.log(data); */
+        })
+        .then(generateMD => {
+          /* console.log(data); */
+          return generateMarkdown(data);
+        })
+        .then(pageReadme => {
+          return writeFile(pageReadme);
+        })
+        .then(writeFileResponse =>{
+          console.log(writeFileResponse.message);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      });
+    });
 }
+
+// function call to initialize program
+init();
