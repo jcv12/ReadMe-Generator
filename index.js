d1729aec5dd1aadfbf8973da3f1eb86bcee9bc9e
jcv12 <john.c.vayianos@gmail.com>
2022-04-28 19:39:55 -0400

    fixing up this code and will use it latyer

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
